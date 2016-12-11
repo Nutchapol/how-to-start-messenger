463e466f7107cff1c36ca9dc62fec70a0aab26f5
Nutchapol <vcy_nut@hotmail.com>
2016-12-11 10:54:13 +0700

    test3

--- server.js
+++ server.js
@@ -17,18 +17,33 @@ app.get('/webhook/', function (req, res) {
   }
   res.send('Error, wrong token')
 })
+
 app.post('/webhook/', function (req, res) {
   let messaging_events = req.body.entry[0].messaging
   for (let i = 0; i < messaging_events.length; i++) {
     let event = req.body.entry[0].messaging[i]
     let sender = event.sender.id
     if (event.message && event.message.text) {
-      let text = event.message.text
+    let text = event.message.text
+    var location = event.message.text
+          var weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&units=metric&appid=7859a2c98ad9c842e6d14f9764d2464a'
+          request({
+            url: weatherEndpoint,
+            json: true
+          }, function(error, response, body) {
+            try {
+              var condition = body.main;
+              sendTextMessage(sender, "Today is " + condition.temp + "Celsius in " + location);
+            } catch(err) {
+              console.error('error caught', err);
+              sendTextMessage(sender, "There was an error.");
+            }
+          }
       if (text === 'Generic') {
         sendGenericMessage(sender)
         continue
       }
-      sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
+      sendTextMessage(sender, 'Text  ' + text.substring(0, 200))
     }
     if (event.postback) {
       let text = JSON.stringify(event.postback)
